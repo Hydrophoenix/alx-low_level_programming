4d8ab850e68b03c8706604eadc7739e25e29bf8d
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-09-08 11:00:08 -0700

    Hello World project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints the string with the printf function
+*
+* Description: using the main function
+* to print "Programming is like building a multilingual puzzle
+* Return 0
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
