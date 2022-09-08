ca6f78c49e5733095f4ae4cb112c8bca61c795dd
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-09-08 15:01:05 -0700

    rework

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main function
 * to print "Programming is like building a multilingual puzzle
-* Return 0
+* Return: 0
 */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,22 @@
 
 /**
 * main - printinting the size of various data types on the computer
-* Return 0
+*
+* Description: using the main function
+* Return: 0
 */
 int main(void)
 {
-        char c;
-        int i;
-        long li;
-        long long lli;
-        float f;
+    char c;
+    int i;
+    long li;
+    long long lli;
+    float f;
 
-        printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float %ld byte(s)\n", sizeof(f));
-	return (0);
+    printf("Size of a char: %ld byte(s)\n", sizeof(c));
+    printf("Size of an int: %ld byte(s)\n", sizeof(i));
+    printf("Size of a long: %ld byte(s)\n", sizeof(li));
+    printf("Size of a long long %ld byte(s)\n", sizeof(lli));
+    printf("Size of a float %ld byte(s)\n", sizeof(f));
+    return (0);
 }
