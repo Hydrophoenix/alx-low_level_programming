8359b158e8945546c4c6f31fa0afeecf0ef7bc82
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-09-15 05:35:15 -0700

    doing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero or negative"
- * Return : 0
+ * Return: 0
 */
 int main(void)
 {
@@ -15,13 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-        if (n > 0) {
+	if (n > 0)
+	{
 		printf("%i is positive\n", n);
 	}
-	else if (n == 0) {
+	else if (n == 0)
+	{
 		printf("%i is zero\n", n);
 	}
-	else if (n < 0) {
+	else if (n < 0)
+	{
 		printf("%i is negative\n", n);
 	}
 	return (0);
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,8 +8,8 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
-int main(void) {
-
+int main(void)
+{
 	int n;
 	int l;
 
@@ -17,10 +17,12 @@ int main(void) {
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 
-	if (l > 5) {
+	if (l > 5)
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (l == 0) {
+	else if (l == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	else
@@ -28,5 +30,4 @@ int main(void) {
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
-
 }
